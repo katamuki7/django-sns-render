9c84c3a5168449010efddaf783faa55efefb8969
fuku <fukuyuu0729@gmail.com>
2024-08-09 20:55:46 +0900

    no message

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin_kanri/", admin.site.urls),
     path("", include("app.urls")),
 ]
